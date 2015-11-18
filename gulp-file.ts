30d535fd80fac95b41fe020fdaadc052dc7a5750
Nick Owen <nowen@scottlogic.com>
2015-11-18 10:19:40 +0000

    Tidy files + directories

--- gulp-file.ts
+++ gulp-file.ts
@@ -1,56 +1,58 @@
 /// <reference path='typings/node/node.d.ts' />
 
 let gulp = require('gulp');
-let del = require('del');
 let ts = require('gulp-typescript');
 let sourcemaps = require('gulp-sourcemaps');
-let tslint = require('gulp-tslint');
-let jsmine = require('gulp-jasmine');
-let concat = require('gulp-concat');
-let rename = require('gulp-rename');
-let uglify = require('gulp-uglify');
 
 let createProject = () => ts.createProject('tsconfig.json');
 
 let paths = {
     src: {
-        app: 'src/app/',
-        js: 'src/js/',
-        maps: './maps/'
+        ts: './src/ts/',
+        js: './src/js/',
+        dist: './dist/'
     },
     tests: {
-        app: 'tests/app/',
-        js: 'tests/js/',
-        maps: './maps/'
-    },
-    dest: 'dist/'
+        ts: './tests/ts/',
+        js: './tests/js/'
+    }
 };
 
 let files = {
-    app: {
-        ts: paths.src.app + '**/*.ts',
-        js: paths.src.js + '**/*.js'
+    src: {
+        ts: paths.src.ts + '**/*.ts',
+        js: paths.src.js + '**/*.js',
+        maps: paths.src.js + '**/*.js.map'
     },
-    test: {
+    tests: {
         specs: {
-            ts: paths.tests.app + '**/*[Ss]pec.ts',
+            ts: paths.tests.ts + '**/*[Ss]pec.ts',
             js: paths.tests.js + '**/*[Ss]pec.js'
         },
-        ts: paths.tests.app + '**/*.ts',
-        js: paths.tests.js + '**/*.*'
+        ts: paths.tests.ts + '**/*.ts',
+        js: paths.tests.js + '**/*.js',
+        maps: paths.tests.js + '**/*.js.map'
     },
     releaseName: 'techstudio.js',
     gulp: 'gulp-file.ts'
 };
 
 gulp.task('lint', () => {
-    return gulp.src([files.app.ts, files.test.ts, files.gulp])
+    let tslint = require('gulp-tslint');
+
+    return gulp.src([files.src.ts, files.tests.ts, files.gulp])
         .pipe(tslint())
         .pipe(tslint.report('verbose'));
 });
 
 gulp.task('clean', () => {
-    return del([files.app.js, paths.dest + '**/*', files.test.js]);
+    let del = require('del');
+
+    let src = [files.src.js, files.src.maps],
+        tests = [files.tests.js, files.tests.maps],
+        dist = [paths.src.dist + '**/*'];
+
+    return del(src.concat(tests).concat(dist));
 });
 
 // Because the tests are transpiling src on the fly cleaning the transpiled src
@@ -58,47 +60,55 @@ gulp.task('clean', () => {
 gulp.task('build:tests', ['lint', 'clean'], () => {
     let tsProject = createProject();
 
-    return gulp.src(files.test.specs.ts)
+    return gulp.src(files.tests.specs.ts)
         .pipe(sourcemaps.init())
         .pipe(ts(tsProject))
-        .pipe(sourcemaps.write(paths.tests.maps))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(paths.tests.js));
 });
 
 // The tests are transpiling the src ts files on the fly, so there is no
 // dependency for building the source yet.
 gulp.task('test', ['build:tests'], () => {
-    return gulp.src(files.test.specs.js)
-        .pipe(jsmine());
+    let jasmine = require('gulp-jasmine');
+
+    return gulp.src(files.tests.specs.js)
+        .pipe(jasmine());
 });
 
 gulp.task('build:src', ['lint', 'clean'], () => {
     let tsProject = createProject();
 
-    return gulp.src(files.app.ts)
+    return gulp.src(files.src.ts)
         .pipe(sourcemaps.init())
         .pipe(ts(tsProject))
-        .pipe(sourcemaps.write(paths.src.maps))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(paths.src.js));
 });
 
 gulp.task('build', ['build:tests', 'build:src']);
 
 gulp.task('concat', ['build', 'test'], () => {
-    return gulp.src(files.app.js)
+    let concat = require('gulp-concat');
+
+    return gulp.src(files.src.js)
         .pipe(concat(files.releaseName))
-        .pipe(gulp.dest(paths.dest));
+        .pipe(gulp.dest(paths.src.dist));
 });
 
-gulp.task('min', ['concat'], () =>
-    gulp.src(paths.dest + files.releaseName)
+gulp.task('min', ['concat'], () => {
+    let rename = require('gulp-rename');
+    let uglify = require('gulp-uglify');
+
+    return gulp.src(paths.src.dist + files.releaseName)
         .pipe(rename({ extname: '.min.js' }))
+        .pipe(sourcemaps.init())
         .pipe(uglify())
-        .pipe(gulp.dest(paths.dest)));
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.src.dist));
+});
 
 gulp.task('watch', () =>
-    gulp.watch(
-        [files.app.ts, files.test.specs.ts, files.gulp],
-        ['lint', 'build', 'test']));
+    gulp.watch([files.src.ts, files.tests.specs.ts, files.gulp], ['lint', 'build', 'test']));
 
 gulp.task('default', ['watch']);
